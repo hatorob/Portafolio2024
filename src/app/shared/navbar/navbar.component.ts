b31694ec661b92565b4b26caa5cefeba22c89e76
Hector Toro <alejotoro94@hotmail.com>
2024-04-12 02:08:29 -0500

    Agrego layout, termino el navbar y la parte inicial de - quien soy - resumen de mi - y ambas cosas ya responsive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Routes {
+  name: string;
+  route: string;
+}
+
 @Component({
   selector: 'shared-navbar',
   templateUrl: './navbar.component.html',
@@ -7,4 +12,14 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  public isClick: boolean = false;
+  public menuHome: Routes[] = [
+    { name: 'Inicio' , route: './inicio' },
+    { name: 'acerca de mi' , route: './acerca-de-mi' },
+    { name: 'Proyectos' , route: './proyectos' },
+    { name: 'Contacto' , route: './contacto' },
+  ];
+  public displayMenu(): void {
+    (this.isClick) ? this.isClick = false : this.isClick = true;
+  }
 }
